c2b378bd50c1010823f0b609e951d18c4a27b856
castrozan <castro.lucas290@gmail.com>
2024-11-04 21:11:42 -0300

    feat: waybar and fuzzel

--- nixos/home/programs/hyprland.nix
+++ nixos/home/programs/hyprland.nix
@@ -1,4 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.file.".config/hypr".source = ../../../.config/hypr;
+
+  # Hyprland packages
+  home.packages = with pkgs; [
+    swaylock-effects
+    swww
+    xclip
+    wl-clipboard
+    gnome.gnome-tweaks
+
+    waybar
+    fuzzel
+  ];
 }
