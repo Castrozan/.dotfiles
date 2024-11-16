f52fdd0cbf9359cc3991076cfc3bc4165077febc
castrozan <castro.lucas290@gmail.com>
2024-11-16 20:18:02 -0300

    fix: fzf history

--- shell/configs/fzf_bash_history.sh
+++ shell/configs/fzf_bash_history.sh
@@ -5,17 +5,17 @@
 # Inspired from https://github.com/junegunn/fzf/wiki/examples#searching-file-contents
 
 bind '"\C-f": "\C-x1\e^\er"'
-bind -x '"\C-x1": __fzf_history'
+bind -x '"\C-x1": fzf_history'
 
-__fzf_history() {
-    __ehc "$(
+fzf_history() {
+    _ehc "$(
         history |
             fzf --tac --tiebreak=index --height 40% --layout=reverse --border |
             perl -ne 'm/^\s*([0-9]+)/ and print "!$1"'
     )"
 }
 
-__ehc() {
+_ehc() {
     if [ -n "$1" ]; then
         bind '"\er": redraw-current-line'
         bind '"\e^": magic-space'
