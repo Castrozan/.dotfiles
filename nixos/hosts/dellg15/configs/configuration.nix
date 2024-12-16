f3e2b1bc0058e26945bd04d5c7e55653323999a9
Castrozan <castro.lucas290@gmail.com>
2024-12-15 22:40:21 -0300

    style: fmt

--- nixos/hosts/dellg15/configs/configuration.nix
+++ nixos/hosts/dellg15/configs/configuration.nix
@@ -1,10 +1,11 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     # Include the results of the hardware scan.
     # Im setting this on hosts/<user>/default.nix
@@ -94,16 +95,15 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-
     # Modesetting is required.
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
     powerManagement.enable = false;
 
@@ -113,12 +113,12 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    # Very laggy so leave false!! 
+    # Very laggy so leave false!!
     open = false;
 
     # Enable the Nvidia settings menu,
@@ -140,13 +140,12 @@
   };
 
   # Enable some experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Set up fonts
   fonts.packages = with pkgs; [
-    
   ];
 
   # Comentarrrr
-  #services.xserver.wacom.enable = true;
+  services.xserver.wacom.enable = true;
 }
