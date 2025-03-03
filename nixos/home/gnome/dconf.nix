e5f34d22b17b3605d7a1556fc5cf848be5df190c
Castrozan <castro.lucas290@gmail.com>
2025-03-03 16:38:24 -0300

    feat: extensions to gnome

--- nixos/home/gnome/dconf.nix
+++ nixos/home/gnome/dconf.nix
@@ -12,6 +12,9 @@ with lib.hm.gvariant;
     "org/gnome/desktop/interface" = {
       gtk-theme = "Adwaita-dark";
       color-scheme = "prefer-dark";
+      cursor-theme = "Adwaita";
+      cursor-size = 24;
+      show-battery-percentage = true;
     };
 
     # "org/gnome/desktop/peripherals/touchpad" = {
@@ -25,9 +28,24 @@ with lib.hm.gvariant;
     #   xkb-options = [ "terminate:ctrl_alt_bksp" "lv3:ralt_switch" "caps:ctrl_modifier" ];
     # };
 
-    # "org/gnome/desktop/screensaver" = {
-    #   picture-uri = "file:///home/gvolpe/Pictures/nixos.png";
-    # };
-  };
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///home/zanoni/.dotfiles/resources/alter-jellyfish-dark.jpg";
+      picture-uri-dark = "file:///home/zanoni/.dotfiles/resources/alter-jellyfish-dark.jpg";
+      primary-color = "#000000000000";
+      secondary-color = "#000000000000";
+    };
 
+    "org/gnome/desktop/screensaver" = {
+      picture-uri = "file:///home/zanoni/.dotfiles/resources/alter-jellyfish-dark.jpg";
+    };
+
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "default-workspace@mateusrodcosta.com"
+        "wsmatrix@martin.zurowietz.de"
+      ];
+    };
+  };
 }
