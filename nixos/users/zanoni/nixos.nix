8e79f2d125e06bf424df2bb92ce52a5f99cc143b
Castrozan <castro.lucas290@gmail.com>
2025-03-20 20:04:33 -0300

    fix: nix_path

--- nixos/users/zanoni/nixos.nix
+++ nixos/users/zanoni/nixos.nix
@@ -1,11 +1,9 @@
 #
 # NixOS Configuration for zanoni
 #
-{ ... }:
-let
+{ lib, ...}: let
   bashrc = builtins.readFile ../../../.bashrc;
-in
-{
+in {
   imports = [
     # TODO: Change this three to be managed as home programs
     ./packages.nix
@@ -20,6 +18,16 @@ in
     xwayland.enable = true;
   };
 
+  environment.variables = {
+    NIX_PATH = lib.mkDefault "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos";
+  };
+
+  # # make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  # # discard all the default paths, and only use the one from this flake.
+  # nix.nixPath = lib.mkForce ["/etc/nix/inputs"];
+  # # https://github.com/NixOS/nix/issues/9574
+  # nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+
   # Global Bash configuration
   # TODO: this is workaround from home/programs/bash.nix
   environment.etc."bashrc".text = bashrc;
@@ -32,6 +40,6 @@ in
       "networkmanager"
       "wheel"
     ];
-    packages = [ ];
+    packages = [];
   };
 }
