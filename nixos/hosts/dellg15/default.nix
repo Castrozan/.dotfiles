9f09ba6130e7db0c8f7afc1e5dcbd4d7d1aa3a83
Castrozan <castro.lucas290@gmail.com>
2025-03-23 23:44:27 -0300

    fix: nvidia config

--- nixos/hosts/dellg15/default.nix
+++ nixos/hosts/dellg15/default.nix
@@ -1,20 +1,15 @@
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [
-      ./configs/configuration.nix
+  imports = [
+    ./configs/configuration.nix
 
-      # NixOS modules
-      # These are configurations that can be done on the system level
-      ../../modules/system.nix
-      ../../modules/game-shift.nix
-      # ../../modules/i3.nix
+    # NixOS modules
+    # These are configurations that can be done on the system level
+    ../../modules/system.nix
+    ../../modules/game-shift.nix
+    # ../../modules/i3.nix
 
-      # Include the results of the hardware scan.
-      # Gen with the command: sudo nixos-generate-config --show-hardware-config
-      ./configs/hardware-configuration.nix
-    ];
+    # Include the results of the hardware scan.
+    # Gen with the command: sudo nixos-generate-config --show-hardware-config
+    ./configs/hardware-configuration.nix
+  ];
 }
-
-
