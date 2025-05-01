d27584cb31de85def143d9803e47347e008462b8
Castrozan <castro.lucas290@gmail.com>
2025-04-30 21:32:12 -0300

    chore: bump vscode

--- nixos/home/programs/vscode.nix
+++ nixos/home/programs/vscode.nix
@@ -4,11 +4,11 @@
 { pkgs, ... }:
 let
   vscode = pkgs.vscode.overrideAttrs (old: rec {
-    version = "1.92.0";
+    version = "1.99.3";
     src = pkgs.fetchurl {
       name = "VSCode_${version}_linux-x64.tar.gz";
       url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
-      sha256 = "ZLTNUk+CFjuWwALSCWL6HOsjPrRgoAvNUUNiyiHOE+E=";
+      sha256 = "tP9frbQweLpJVhiBbwIvuFm87IHCC7LT9pNT3V10ODU=";
     };
   });
 in
