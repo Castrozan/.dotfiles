a6fc1a370f1b77ba6bef74ddce87240b425ab35b
Castrozan <castro.lucas290@gmail.com>
2025-05-12 20:58:41 -0300

    feat: merge without nvidia changes

---
+++ nix-home-ubuntu/home.nix
@@ -0,0 +1,28 @@
+{ pkgs, username, home-version, ... }:
+{
+	home.username = username;
+	home.homeDirectory = "/home/${username}";
+	home.stateVersion = home-version;
+	programs.home-manager.enable = true;
+	news.display = "silent";
+
+	imports = [
+		./modules/pipx.nix
+		./modules/dooit.nix
+		./modules/sdkman.nix
+		./modules/m2.nix
+	];
+
+	home.packages = with pkgs; [
+		git
+		xclip
+		curl
+		zip
+		unzip
+		gnutar
+		curl
+
+		insomnia
+		uv
+	];
+}
