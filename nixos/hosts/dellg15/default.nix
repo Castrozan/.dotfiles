9fb831b5dd278a3b922e4876b869c95c1a2ed278
Castrozan <castro.lucas290@gmail.com>
2025-05-31 15:47:13 -0300

    refactor: modules

--- nixos/hosts/dellg15/default.nix
+++ nixos/hosts/dellg15/default.nix
@@ -1,14 +1,14 @@
 {
   imports = [
+    # Main host configurations
     ./configs/configuration.nix
-
-    # NixOS modules
-    # These are configurations that can be done on the system level
-    ../../modules/game-shift.nix
-    # ../../modules/i3.nix
 
     # Include the results of the hardware scan.
     # Gen with the command: sudo nixos-generate-config --show-hardware-config
     ./configs/hardware-configuration.nix
+
+    # NixOS modules
+    # These are configurations that can be done on the system level
+    ../../modules/game-shift.nix
   ];
 }
