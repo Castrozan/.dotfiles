ab83025176b6999001a9b4d232c6e261346ec406
Castrozan <castro.lucas290@gmail.com>
2025-06-29 17:03:30 -0300

    fix: remove zen

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -23,10 +23,10 @@
       url = "github:castrozan/codex-flake";
       inputs.flake-utils.follows = "flake-utils";
     };
-    zen-browser = {
-      url = "github:MarceColl/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #zen-browser = {
+    #  url = "github:MarceColl/zen-browser-flake";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 
   outputs =
