c4eb447441cf26b0c43571c11d1fca900ebc072a
Castrozan <castro.lucas290@gmail.com>
2025-06-29 17:03:30 -0300

    fix: remove zen

--- nixos/users/zanoni/system-packages.nix
+++ nixos/users/zanoni/system-packages.nix
@@ -4,6 +4,6 @@
   environment.systemPackages = [
     #inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
     inputs.codex-flake.packages.${pkgs.system}.default
-    inputs.zen-browser.packages.${pkgs.system}.default
+    #inputs.zen-browser.packages.${pkgs.system}.default
   ];
 }
