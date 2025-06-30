980181683a5964c613fa7f5c28e8651e9ca8f2c4
Castrozan <castro.lucas290@gmail.com>
2025-06-29 23:25:08 -0300

    fix: nixos tmux conf

--- nixos/home/programs/tmux.nix
+++ nixos/home/programs/tmux.nix
@@ -2,20 +2,33 @@
 let
   settings = builtins.readFile ../../../.config/tmux/settings.conf;
   binds = builtins.readFile ../../../.config/tmux/binds.conf;
+  pluginSettings = builtins.readFile ../../../.config/tmux/plugin-settings.conf;
+
+  catppuccinZanoni = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "zanoni.v1.0.3";
+    src = pkgs.fetchFromGitHub {
+      owner = "castrozan";
+      repo = "tmux";
+      rev = "zanoni.v1.0.3";
+      sha256 = "0aYa1LY8BRGkU3Cd1l7dmLnGsFXbbRjmCtInYfiEwTA=";
+    };
+  };
 in
 {
   programs.tmux = {
     enable = true;
     clock24 = false;
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      yank
-      catppuccin
-      resurrect
+    plugins = [
+      pkgs.tmuxPlugins.sensible
+      pkgs.tmuxPlugins.yank
+      pkgs.tmuxPlugins.resurrect
+      catppuccinZanoni
     ];
     extraConfig = ''
       ${settings}
       ${binds}
+      ${pluginSettings}
     '';
   };
 }
