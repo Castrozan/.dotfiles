96cb1207e94872d3ae5bcc3fd1d79ba48b8c9d81
Castrozan <castro.lucas290@gmail.com>
2025-07-19 18:13:21 -0300

    feat: delta pager on git

--- nixos/home/programs/git.nix
+++ nixos/home/programs/git.nix
@@ -1,10 +1,21 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.gh ];
+  home.packages = [
+    pkgs.gh
+    pkgs.delta
+  ];
 
   programs.git = {
     enable = true;
     userName = "Castrozan";
     userEmail = "castro.lucas290@gmail.com";
+
+    extraConfig = {
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      delta.navigate = true; # use n/N to jump hunks
+      delta.dark = true; # force dark theme
+      merge.conflictstyle = "zdiff3";
+    };
   };
 }
