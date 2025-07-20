fe06a5a8f6c3b201ca9c95d56641a4384cbddf8d
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-07-20 18:14:01 -0300

    refactor: home pkgs to modules

--- nixos/users/zanoni/home.nix
+++ nixos/users/zanoni/home.nix
@@ -6,7 +6,16 @@
     ../../home/modules/hyprland
     ../../home/modules/gnome
     ../../home/modules/kitty.nix
-    ../../home/packages
     ../../home/modules/vscode
+    ../../home/modules/common.nix
+    ../../home/modules/git.nix
+    ../../home/modules/bash.nix
+    ../../home/modules/pkgs.nix
+    ../../home/modules/neovim.nix
+    ../../home/modules/tmux.nix
+    ../../home/modules/fuzzel.nix
+    ../../home/modules/playwright.nix
+    ../../home/modules/vesktop.nix
+    ../../home/modules/lazygit.nix
   ];
 }
