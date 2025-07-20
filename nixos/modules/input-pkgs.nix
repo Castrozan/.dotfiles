9439533c5fafc00db44046008d2a170d5eb5a8de
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-07-20 17:16:51 -0300

    refac: move files to home/

--- nixos/modules/input-pkgs.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, ... }:
-{
-  # List of flake input packages
-  environment.systemPackages = [
-    #inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
-    inputs.codex-flake.packages.${pkgs.system}.default
-    #inputs.zen-browser.packages.${pkgs.system}.default
-  ];
-}
