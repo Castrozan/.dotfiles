9ceb62c41aa601e3b3eedb4488203238a20cf64b
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-07-20 23:09:33 -0300

    feat: neovim

--- nixos/users/lucas.zanoni/home.nix
+++ nixos/users/lucas.zanoni/home.nix
@@ -1,15 +1,12 @@
-# All Zanoni's Home Manager Configuration
 {
   imports = [
     ../../home/core.nix
     ./pkgs.nix
 
-    # ../../home/hyprland
     ../../home/modules/gnome
+    ../../home/modules/neovim.nix
     ../../home/modules/kitty.nix
     ../../home/modules/tmux.nix
     ../../home/modules/lazygit.nix
-    # ../../home/packages
-    # ../../home/vscode
   ];
 }
