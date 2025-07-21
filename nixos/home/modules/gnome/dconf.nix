3c88e2eec88428038b84995d7f98ccb14f38a5bd
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-07-21 14:47:43 -0300

    feat: flameshot bindings

--- nixos/home/modules/gnome/dconf.nix
+++ nixos/home/modules/gnome/dconf.nix
@@ -75,7 +75,14 @@ in
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
       ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "flameshot";
+      binding = "Print";
+      command = "flameshot gui";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
