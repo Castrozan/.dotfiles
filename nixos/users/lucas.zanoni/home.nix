66f16f9acd1c9e3e7e7015dd784c217c66eadb7d
Castrozan <castro.lucas290@gmail.com>
2025-07-25 15:53:12 -0300

    feat: k9s
    a
    
    a

--- nixos/users/lucas.zanoni/home.nix
+++ nixos/users/lucas.zanoni/home.nix
@@ -12,6 +12,7 @@
     ../../home/modules/tmux.nix
     ../../home/modules/lazygit.nix
     ../../home/modules/dooit.nix
+    ../../home/modules/k9s.nix
     ../../home/modules/pipx.nix
     ../../home/modules/sdkman.nix
     ../../home/modules/gnome/dconf.nix
