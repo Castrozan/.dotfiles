f49138eca6def40954a9f5e3101e4ee3db9f056e
Castrozan <castro.lucas290@gmail.com>
2025-08-01 13:29:33 -0300

    refactor: aliases and nix files

--- shell/configs/fish/conf.d/default_directories.fish
+++ shell/configs/fish/conf.d/default_directories.fish
@@ -1,16 +1,12 @@
-# Default directories for the shell
-
-# Create repo directory if it doesn't exist
+# TODO: this should not be done in the shell
 if test ! -d "$HOME/repo"
     mkdir -p "$HOME/repo"
 end
 
-# Create satc directory if it doesn't exist
 if test ! -d "$HOME/repo/satc"
     mkdir -p "$HOME/repo/satc"
 end
 
-# Create fonts directory if it doesn't exist
 if test ! -d "$HOME/.local/share/fonts"
     mkdir -p "$HOME/.local/share/fonts"
 end
