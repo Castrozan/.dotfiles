86837758985850d2dd9700b309ac72d081843342
Castrozan <castro.lucas290@gmail.com>
2025-08-01 13:29:33 -0300

    refactor: aliases and nix files

--- nixos/home/modules/fish.nix
+++ nixos/home/modules/fish.nix
@@ -13,16 +13,4 @@ in
     package = pkgs.fish;
     interactiveShellInit = ''${shellInit}'';
   };
-
-  home.file.".config/fish/bass_env.fish".source = ../../../shell/configs/fish/bass_env.fish;
-  home.file.".config/fish/conf.d/tmux.fish".source = ../../../shell/configs/fish/conf.d/tmux.fish;
-  home.file.".config/fish/conf.d/fzf.fish".source = ../../../shell/configs/fish/conf.d/fzf.fish;
-  home.file.".config/fish/conf.d/aliases.fish".source =
-    ../../../shell/configs/fish/conf.d/aliases.fish;
-  home.file.".config/fish/conf.d/default_directories.fish".source =
-    ../../../shell/configs/fish/conf.d/default_directories.fish;
-  home.file.".config/fish/functions/fish_prompt.fish".source =
-    ../../../shell/configs/fish/functions/fish_prompt.fish;
-  home.file.".config/fish/functions/screensaver.fish".source =
-    ../../../shell/configs/fish/functions/screensaver.fish;
 }
