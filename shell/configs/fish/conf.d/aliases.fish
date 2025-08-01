9c697269084adfe8111341c75b8ce716515d056c
Castrozan <castro.lucas290@gmail.com>
2025-08-01 15:45:13 -0300

    fix: sdkman for fish

--- shell/configs/fish/conf.d/aliases.fish
+++ shell/configs/fish/conf.d/aliases.fish
@@ -2,3 +2,9 @@ source ~/.dotfiles/shell/configs/bash_aliases.sh
 source ~/.dotfiles/.bash_env_vars
 
 alias source-shell 'source ~/.dotfiles/shell/configs/fish/config.fish'
+
+function wildfly
+  sdk use java 8.0.432-amzn
+  cd /opt/wildfly-aplicacoes/bin
+  ./standalone.sh $argv
+end
