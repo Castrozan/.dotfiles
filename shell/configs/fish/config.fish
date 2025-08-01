f3009f7eefa2353327974b743686a1f9819dade7
Castrozan <castro.lucas290@gmail.com>
2025-08-01 14:43:28 -0300

    feat: source cursor functions

--- shell/configs/fish/config.fish
+++ shell/configs/fish/config.fish
@@ -12,5 +12,6 @@ source ~/.dotfiles/shell/configs/fish/conf.d/default_directories.fish
 # Functions
 source ~/.dotfiles/shell/configs/fish/functions/fish_prompt.fish
 source ~/.dotfiles/shell/configs/fish/functions/screensaver.fish
+source ~/.dotfiles/shell/configs/fish/functions/cursor.fish
 
 zoxide init fish | source
