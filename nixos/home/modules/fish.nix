7c43ca356028afbdbf2e430988adebcf5dd40a80
Castrozan <castro.lucas290@gmail.com>
2025-08-01 12:55:11 -0300

    feat: aliases and config to pkgs

--- nixos/home/modules/fish.nix
+++ nixos/home/modules/fish.nix
@@ -3,12 +3,20 @@
   programs.fish = {
     enable = true;
     package = pkgs.fish;
+    extraConfig = builtins.readFile ../../../shell/configs/fish/config.fish;
   };
 
   # Link the new fish config files to the correct location
-  #   home.file.".config/fish/config.fish".source = ../../../shell/configs/fish/config.fish;
+  # home.file.".config/fish/config.fish".source is not needed due to extraConfig
   home.file.".config/fish/bass_env.fish".source = ../../../shell/configs/fish/bass_env.fish;
   home.file.".config/fish/conf.d/tmux.fish".source = ../../../shell/configs/fish/conf.d/tmux.fish;
+  home.file.".config/fish/conf.d/fzf.fish".source = ../../../shell/configs/fish/conf.d/fzf.fish;
+  home.file.".config/fish/conf.d/aliases.fish".source =
+    ../../../shell/configs/fish/conf.d/aliases.fish;
+  home.file.".config/fish/conf.d/default_directories.fish".source =
+    ../../../shell/configs/fish/conf.d/default_directories.fish;
   home.file.".config/fish/functions/fish_prompt.fish".source =
     ../../../shell/configs/fish/functions/fish_prompt.fish;
+  home.file.".config/fish/functions/screensaver.fish".source =
+    ../../../shell/configs/fish/functions/screensaver.fish;
 }
--- shell/configs/fish/config.fish
+++ shell/configs/fish/config.fish
@@ -4,6 +4,11 @@ if status is-interactive
   source ~/.dotfiles/shell/configs/fish/bass_env.fish
 
   # Fish-native components
-  # source ~/.dotfiles/shell/configs/fish/conf.d/tmux.fish
-  # zoxide init fish | source
+  source ~/.dotfiles/shell/configs/fish/conf.d/tmux.fish
+  source ~/.dotfiles/shell/configs/fish/conf.d/aliases.fish
+  source ~/.dotfiles/shell/configs/fish/conf.d/fzf.fish
+  source ~/.dotfiles/shell/configs/fish/conf.d/default_directories.fish
+  zoxide init fish | source
 end
+
+set fish_greeting
