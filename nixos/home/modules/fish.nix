8a1f7221a15cf093fee0c005eebd63435a3295f2
Castrozan <castro.lucas290@gmail.com>
2025-08-01 12:42:59 -0300

    feat: fish config integration

--- nixos/home/modules/fish.nix
+++ nixos/home/modules/fish.nix
@@ -3,10 +3,12 @@
   programs.fish = {
     enable = true;
     package = pkgs.fish;
-    shellAliases = {
-      ls = "eza --icons=always";
-      ll = "eza --icons=always -l";
-      la = "eza --icons=always -la";
-    };
   };
+
+  # Link the new fish config files to the correct location
+  #   home.file.".config/fish/config.fish".source = ../../../shell/configs/fish/config.fish;
+  home.file.".config/fish/bass_env.fish".source = ../../../shell/configs/fish/bass_env.fish;
+  home.file.".config/fish/conf.d/tmux.fish".source = ../../../shell/configs/fish/conf.d/tmux.fish;
+  home.file.".config/fish/functions/fish_prompt.fish".source =
+    ../../../shell/configs/fish/functions/fish_prompt.fish;
 }
