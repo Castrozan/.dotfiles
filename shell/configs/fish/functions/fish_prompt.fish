4f7b9fe62791eaa15d39288c3442edde1f1db4ef
Castrozan <castro.lucas290@gmail.com>
2025-08-01 14:36:36 -0300

    feat: bold fish prompt

--- shell/configs/fish/functions/fish_prompt.fish
+++ shell/configs/fish/functions/fish_prompt.fish
@@ -1,9 +1,9 @@
 function fish_prompt
-  set_color green
+  set_color --bold green
   printf ' %s ' (whoami)
-  set_color blue
+  set_color --bold blue
   printf '%s' (prompt_pwd)
-  set_color yellow
+  set_color --bold yellow
   printf '%s' (fish_git_prompt)
   set_color normal
   printf '$ '
