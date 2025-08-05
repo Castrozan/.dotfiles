9014bdadbbf3e0f689ffc89959ceac27f7d57edf
Castrozan <castro.lucas290@gmail.com>
2025-08-05 10:35:00 -0300

    feat: nvm on fish

--- shell/configs/fish/config.fish
+++ shell/configs/fish/config.fish
@@ -10,6 +10,7 @@ source ~/.dotfiles/shell/configs/fish/bass_env.fish
 source ~/.dotfiles/shell/configs/fish/conf.d/tmux.fish
 source ~/.dotfiles/shell/configs/fish/conf.d/aliases.fish
 source ~/.dotfiles/shell/configs/fish/conf.d/fzf.fish
+source ~/.dotfiles/shell/configs/fish/conf.d/nvm.fish
 source ~/.dotfiles/shell/configs/fish/conf.d/default_directories.fish
 source ~/.dotfiles/shell/configs/fish/conf.d/key_bindings.fish
 
