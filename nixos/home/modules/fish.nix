f87ced20b1ed234fb670214e23a8d9f9e34102dc
Castrozan <castro.lucas290@gmail.com>
2025-08-11 11:50:04 -0300

    feat: add bass globally

--- nixos/home/modules/fish.nix
+++ nixos/home/modules/fish.nix
@@ -3,13 +3,23 @@ let
   shellInit = builtins.readFile ../../../shell/configs/fish/config.fish;
 in
 {
+  home.packages = with pkgs; [
+    fishPlugins.bass
+  ];
+
   programs.fish = {
     enable = true;
     package = pkgs.fish;
     interactiveShellInit = ''${shellInit}'';
     plugins = [
-      { name = "bass"; src = pkgs.fishPlugins.bass; }
-      { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish; }
+      {
+        name = "bass";
+        src = pkgs.fishPlugins.bass;
+      }
+      {
+        name = "fzf-fish";
+        src = pkgs.fishPlugins.fzf-fish;
+      }
     ];
   };
 }
