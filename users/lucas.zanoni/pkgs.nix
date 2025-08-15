cd19de4b428af466249ab67cba05c65524c66263
Castrozan <castro.lucas290@gmail.com>
2025-08-15 18:15:47 -0300

    refactor(lucas.zanoni): use cursor from latest

--- users/lucas.zanoni/pkgs.nix
+++ users/lucas.zanoni/pkgs.nix
@@ -51,18 +51,19 @@
     # Appending to list
     ++ (with latest; [
       claude-code
-      (code-cursor.overrideAttrs (old: {
-        version = "1.4.2";
-        src = pkgs.appimageTools.extract {
-          pname = "code-cursor";
-          version = "1.4.2";
-          src = pkgs.fetchurl {
-            url = "https://downloads.cursor.com/production/d01860bc5f5a36b62f8a77cd42578126270db343/linux/x64/Cursor-1.4.2-x86_64.AppImage";
-            sha256 = "sha256-WMZA0CjApcSTup4FLIxxaO7hMMZrJPawYsfCXnFK4EE=";
-          };
-        };
-        sourceRoot = "code-cursor-1.4.2-extracted/usr/share/cursor";
-      }))
+      # (code-cursor.overrideAttrs (old: {
+      #   version = "1.4.2";
+      #   src = pkgs.appimageTools.extract {
+      #     pname = "code-cursor";
+      #     version = "1.4.2";
+      #     src = pkgs.fetchurl {
+      #       url = "https://downloads.cursor.com/production/d01860bc5f5a36b62f8a77cd42578126270db343/linux/x64/Cursor-1.4.2-x86_64.AppImage";
+      #       sha256 = "sha256-WMZA0CjApcSTup4FLIxxaO7hMMZrJPawYsfCXnFK4EE=";
+      #     };
+      #   };
+      #   sourceRoot = "code-cursor-1.4.2-extracted/usr/share/cursor";
+      # }))
+      code-cursor
       devenv
       direnv
       # gemini-cli TODO: fix gemini-cli, im using the npm version for now
