ca631efffb9673046a8d8398c56afd69e7166b5b
Castrozan <castro.lucas290@gmail.com>
2025-08-18 19:17:20 -0300

    refactor: cursor

--- home/modules/cursor.nix
+++ home/modules/cursor.nix
@@ -2,17 +2,30 @@
 {
   home.packages = with latest; [
     # code-cursor
-    (code-cursor.overrideAttrs (old: {
-      version = "1.4.5";
-      src = pkgs.appimageTools.extract {
-        pname = "code-cursor";
+    (code-cursor.overrideAttrs (
+      old:
+      let
         version = "1.4.5";
-        src = pkgs.fetchurl {
-          url = "https://downloads.cursor.com/production/af58d92614edb1f72bdd756615d131bf8dfa5299/linux/x64/Cursor-1.4.5-x86_64.AppImage";
-          sha256 = "sha256-2Hz1tXC+YkIIHWG1nO3/84oygH+wvaUtTXqvv19ZAz4=";
+        sha256 = "sha256-2Hz1tXC+YkIIHWG1nO3/84oygH+wvaUtTXqvv19ZAz4=";
+        buildId = "af58d92614edb1f72bdd756615d131bf8dfa5299";
+      in
+      {
+        version = version;
+        src = pkgs.appimageTools.extract {
+          pname = "code-cursor";
+          version = version;
+          src = pkgs.fetchurl {
+            sha256 = sha256;
+            url =
+              "https://downloads.cursor.com/production/"
+              + buildId
+              + "/linux/x64/Cursor-"
+              + version
+              + "-x86_64.AppImage";
+          };
         };
-      };
-      sourceRoot = "code-cursor-1.4.5-extracted/usr/share/cursor";
-    }))
+        sourceRoot = "code-cursor-" + version + "-extracted/usr/share/cursor";
+      }
+    ))
   ];
 }
