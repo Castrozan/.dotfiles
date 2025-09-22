e55fec62935b518497ae1ac4b8ae57e99c1b4ef9
Castrozan <castro.lucas290@gmail.com>
2025-09-22 18:54:07 -0300

    chore: cursor

--- home/modules/cursor.nix
+++ home/modules/cursor.nix
@@ -1,31 +1,31 @@
 { latest, ... }:
 {
   home.packages = with latest; [
-    # code-cursor
-    (code-cursor.overrideAttrs (
-      old:
-      let
-        version = "1.4.5";
-        sha256 = "sha256-2Hz1tXC+YkIIHWG1nO3/84oygH+wvaUtTXqvv19ZAz4=";
-        buildId = "af58d92614edb1f72bdd756615d131bf8dfa5299";
-      in
-      {
-        version = version;
-        src = pkgs.appimageTools.extract {
-          pname = "code-cursor";
-          version = version;
-          src = pkgs.fetchurl {
-            sha256 = sha256;
-            url =
-              "https://downloads.cursor.com/production/"
-              + buildId
-              + "/linux/x64/Cursor-"
-              + version
-              + "-x86_64.AppImage";
-          };
-        };
-        sourceRoot = "code-cursor-" + version + "-extracted/usr/share/cursor";
-      }
-    ))
+    code-cursor
+    # (code-cursor.overrideAttrs (
+    #   old:
+    #   let
+    #     version = "1.6.35";
+    #     buildId = "b753cece5c67c47cb5637199a5a5de2b7100c18f";
+    #     sha256 = "sha256-62u8snx9nbJtsg7uROZNVzo3macrkTghTCep943e8+I=";
+    #   in
+    #   {
+    #     version = version;
+    #     src = pkgs.appimageTools.extract {
+    #       pname = "code-cursor";
+    #       version = version;
+    #       src = pkgs.fetchurl {
+    #         sha256 = sha256;
+    #         url =
+    #           "https://downloads.cursor.com/production/"
+    #           + buildId
+    #           + "/linux/x64/Cursor-"
+    #           + version
+    #           + "-x86_64.AppImage";
+    #       };
+    #     };
+    #     sourceRoot = "code-cursor-" + version + "-extracted/usr/share/cursor";
+    #   }
+    # ))
   ];
 }
