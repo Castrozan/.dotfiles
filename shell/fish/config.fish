94d95481f49b16cf7bab4e9aa34e1d29341b1dea
Castrozan <castro.lucas290@gmail.com>
2025-09-24 20:54:11 -0300

    feat: config nss for lucas.zanoni user

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -3,6 +3,13 @@ set fish_greeting
 # Enable autosuggestions
 set -g fish_autosuggestion_enabled 1
 
+# TODO: this should be sourced from a lucas.zanoni specific file on it's config dir
+# NSS library preload for corporate authentication (required for devenv)
+# Only set for lucas.zanoni user
+if test "$USER" = "lucas.zanoni"
+    set -x LD_PRELOAD '/lib/x86_64-linux-gnu/libnss_sss.so.2'
+end
+
 # Portable environment setup
 source ~/.dotfiles/shell/fish/bass_env.fish
 
