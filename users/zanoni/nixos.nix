9ea37ea581994c686f578357772f6bd922adc4cb
Castrozan <castro.lucas290@gmail.com>
2025-10-28 19:39:16 -0300

    Revert "fix: disable xwayland"
    This reverts commit d3b8b34235d4e58c3e65c6b84a5533d0caac327d.

--- users/zanoni/nixos.nix
+++ users/zanoni/nixos.nix
@@ -34,6 +34,7 @@ in
   # More hyprland configuration in home/hyprland.nix
   programs.hyprland = {
     enable = true;
+    xwayland.enable = true;
   };
 
   # Enable fish globally so it's registered in /etc/shells and available as a login shell
