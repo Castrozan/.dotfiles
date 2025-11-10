97958fcd8d3da2c5386779ef529918bd04302e38
Castrozan <castro.lucas290@gmail.com>
2025-11-10 19:41:02 -0300

    docs: workaround for alsa

--- home/modules/vscode/dependencies/vscode-speech.nix
+++ home/modules/vscode/dependencies/vscode-speech.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
 {
+  # Ensure ALSA libraries are available for any speech/voice extensions.
+  #
+  # Note: some voice-identification or microphone-based tools expect ALSA's
+  # default device to be configured in the user's `~/.asoundrc`. If your mic
+  # is on ALSA card 2, for example, create `~/.asoundrc` with:
+  #
+  # defaults.pcm.card 2
+  # defaults.ctl.card 2
+  #
+  # You can manage this file via Home Manager in your `users/<you>/home.nix`:
+  #
+  # home.file.".asoundrc".text = ''
+  # defaults.pcm.card 2
+  # defaults.ctl.card 2
+  # '';
+
   home.packages = with pkgs; [
     alsa-lib
   ];
