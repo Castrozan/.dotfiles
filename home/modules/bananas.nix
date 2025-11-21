11ab114b350a47acb9afd858d0113f5a9085fda6
Castrozan <castro.lucas290@gmail.com>
2025-11-20 21:20:06 -0300

    feat: bananas

---
+++ home/modules/bananas.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+let
+  version = "0.0.22";
+  sha256 = "1h5c21i5rdzsrivzdwknwk18zvvrgmghi1kdamnpsdd9209bsdkj";
+in
+{
+  home.packages = [
+    (pkgs.appimageTools.wrapType2 {
+      pname = "bananas";
+      version = version;
+      src = pkgs.fetchurl {
+        url = "https://github.com/mistweaverco/bananas/releases/download/v${version}/bananas_x86_64.AppImage";
+        sha256 = sha256;
+      };
+    })
+  ];
+}
