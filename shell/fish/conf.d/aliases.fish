d0d1544f01ed39def894db1f13f6f379d261b926
Castrozan <castro.lucas290@gmail.com>
2025-12-01 19:22:17 -0300

    fix: migrate to shell env vars

--- shell/fish/conf.d/aliases.fish
+++ shell/fish/conf.d/aliases.fish
@@ -5,9 +5,3 @@ if test -f ~/.dotfiles/.shell_env_vars
 end
 
 alias source-shell 'source ~/.dotfiles/shell/fish/config.fish'
-
-function wildfly
-  sdk use java 8.0.432-amzn
-  cd /opt/wildfly-aplicacoes/bin
-  ./standalone.sh $argv
-end
