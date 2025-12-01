18686ed4d00af8806019192f3aa2d26b87a20f06
Castrozan <castro.lucas290@gmail.com>
2025-12-01 18:18:14 -0300

    lint: disble for unused variable on file

--- shell/screensaver/config.sh
+++ shell/screensaver/config.sh
@@ -3,6 +3,7 @@
 # List of commands to run in screensaver (in order)
 # First command gets full pane, subsequent commands split horizontally
 # Commands include all arguments - no wrapper functions needed
+# shellcheck disable=SC2034
 SCREENSAVER_COMMANDS=(
     'cbonsai -l -i -b 1 -c mWmW,wMwM,mMw -M 2 --life 35 -o "🎄,⭐,🎁,🔔" -f 60'
     'install-nothing'
