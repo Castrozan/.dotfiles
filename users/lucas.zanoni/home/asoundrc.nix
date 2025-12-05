eeff13d2bc73285b4f0517b7a695e6b1b2fcd091
Castrozan <castro.lucas290@gmail.com>
2025-12-05 14:34:18 -0300

    docs(lucas.zanoni): alsa

--- users/lucas.zanoni/home/asoundrc.nix
+++ users/lucas.zanoni/home/asoundrc.nix
@@ -1,7 +1,19 @@
 { ... }:
 {
   # Ensure ALSA libraries are available for any speech/voice extensions.
-  # See home/modules/vscode/dependencies/vscode-speech.nix for more details.
+  #
+  # arecord -l # list available audio devices
+  #
+  # Note: some voice-identification or microphone-based tools expect ALSA's
+  # default device to be configured in the user's `~/.asoundrc`. If your mic
+  # is on ALSA card 0, for example, create `~/.asoundrc` with:
+  #
+  # You can manage this file via Home Manager in your `users/<you>/home.nix`:
+  #
+  # home.file.".asoundrc".text = ''
+  # defaults.pcm.card 0
+  # defaults.ctl.card 0
+  # '';
   home.file.".asoundrc".text = ''
     defaults.pcm.card 0
     defaults.ctl.card 0
