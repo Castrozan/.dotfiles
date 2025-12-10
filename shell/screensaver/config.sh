4e43142f165df41cf4c87dbbc03400b47ae5856d
Castrozan <castro.lucas290@gmail.com>
2025-12-10 14:45:59 -0300

    feat(lucas.zanoni): upgrade install-nothing and exclude xorg stage from screensaver

--- shell/screensaver/config.sh
+++ shell/screensaver/config.sh
@@ -6,7 +6,7 @@
 # shellcheck disable=SC2034
 SCREENSAVER_COMMANDS=(
     'cbonsai -l -i -b 1 -c mWmW,wMwM,mMw -M 2 --life 35 -o "🎄,⭐,🎁,🔔" -f 60'
-    'install-nothing'
+    'install-nothing --all --exclude xorg'
     'cmatrix -U "🎄,⭐,🎁,🔔" -F 10'
 )
 
