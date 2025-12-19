6324ccba025560d9bf8fd758a5229d531c481823
Castrozan <castro.lucas290@gmail.com>
2025-12-19 00:56:35 -0300

    feat: cleber user

---
+++ users/cleber/home.nix
@@ -0,0 +1,19 @@
+{
+  imports = [
+    ./pkgs.nix
+
+    ../../home/core.nix
+    ../../home/scripts
+    ../../home/modules/cursor
+    ../../home/modules/flameshot.nix
+    ../../home/modules/fish.nix
+    ../../home/modules/k9s.nix
+    ../../home/modules/kitty.nix
+    ../../home/modules/lazygit.nix
+    ../../home/modules/tmux.nix
+    ../../home/modules/vial.nix
+    ../../home/modules/cbonsai.nix
+    ../../home/modules/cmatrix.nix
+    ../../home/modules/install-nothing.nix
+  ];
+}
