aa1d54ea52f851de1e61407c0157f9774917c665
Castrozan <castro.lucas290@gmail.com>
2025-12-30 09:40:07 -0300

    feat: julia bonsai

--- shell/screensaver/config.sh
+++ shell/screensaver/config.sh
@@ -5,7 +5,7 @@
 # Commands include all arguments - no wrapper functions needed
 # shellcheck disable=SC2034
 SCREENSAVER_COMMANDS=(
-    'cbonsai -l -i -b 1 -c mWmW,wMwM,mMw -M 2 --life 35 -o "🎄,⭐,🎁,🔔" -f 60'
+    'cbonsai -l -i -b 1 -c julia,JULIA,JuLiA -M 2 --life 35 -o "🎄,⭐,🎁,🔔" -f 60'
     'install-nothing --all --exclude deno'
     'cmatrix -U "🎄,⭐,🎁,🔔" -F 10'
 )
