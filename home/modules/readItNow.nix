e555b15348aacf911da33ca1a38bb61a4d78c483
Castrozan <castro.lucas290@gmail.com>
2026-01-06 02:57:46 -0300

    fix: system from pkgs on 25.11

--- home/modules/readItNow.nix
+++ home/modules/readItNow.nix
@@ -1,6 +1,6 @@
 { pkgs, inputs, ... }:
 {
   home.packages = [
-    inputs.readItNow-rc.packages.${pkgs.system}.default
+    inputs.readItNow-rc.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
