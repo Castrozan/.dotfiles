82d087c1d19487e003455b557d427bc8a68d445c
Castrozan <castro.lucas290@gmail.com>
2026-01-07 18:29:48 -0300

    chore: screensaver to remove xmas theme

--- shell/screensaver/config.sh
+++ shell/screensaver/config.sh
@@ -2,11 +2,10 @@
 
 # List of commands to run in screensaver (in order)
 # First command gets full pane, subsequent commands split horizontally
-# Commands include all arguments - no wrapper functions needed
 # shellcheck disable=SC2034
 SCREENSAVER_COMMANDS=(
-    'cbonsai -l -i -b 1 -c mWmW,wMwM,mMw -M 2 --life 35 -o "🎄,⭐,🎁,🔔" -f 60'
+    'cbonsai -l -i -b 1 -c mWmW,wMwM,mMw -M 2 --life 35 -f 60'
+    'cmatrix'
     'install-nothing --all --exclude deno'
-    'cmatrix -U "🎄,⭐,🎁,🔔" -F 10'
 )
 
