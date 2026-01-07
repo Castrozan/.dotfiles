4401fb19f7dd5a6e9357125a47c7316e9faf84bf
Castrozan <castro.lucas290@gmail.com>
2026-01-07 11:07:41 -0300

    fix(lucas.zanoni): config for nixpkgs 25.11

--- users/lucas.zanoni/home/ssh.nix
+++ users/lucas.zanoni/home/ssh.nix
@@ -1,7 +1,9 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = { };
       "gitlab.com" = {
         hostname = "gitlab.service.betha.cloud";
         user = "git";
