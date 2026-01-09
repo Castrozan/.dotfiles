d42cf4d9c44b3db432f805f99cec54344b360721
Castrozan <castro.lucas290@gmail.com>
2026-01-09 18:43:41 -0300

    fix: daily note keybind

--- home/modules/gnome/dconf.nix
+++ home/modules/gnome/dconf.nix
@@ -90,7 +90,7 @@ in
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/"
       ];
@@ -108,7 +108,7 @@ in
       command = "xdg-open 'obsidian://adv-uri?commandid=obsidian-read-it-later%3Asave-clipboard-to-notice'";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
       name = "daily-note";
       binding = "<Super>d";
       command = "daily-note";
