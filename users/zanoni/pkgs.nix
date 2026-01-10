999e6aa4f541aef4da36208f6347219c5ffd2d16
Castrozan <castro.lucas290@gmail.com>
2026-01-10 02:05:17 -0300

    refactor(zanoni)(zanoni): pkgs

--- users/zanoni/pkgs.nix
+++ users/zanoni/pkgs.nix
@@ -9,104 +9,72 @@
   environment.systemPackages =
     with pkgs;
     [
-      # Development Tools
       alejandra
       awscli2
-      azure-cli
-      dbeaver-bin
-      cacert
-      cypress
-      docker
-      gh
-      git
-      gnumake
-      just
-      k6
-      lazydocker
-      nixd
-      nixpkgs-fmt
-      nix-prefetch-github
-      nodePackages.prettier
-      # terraform # Removed: its broken on nixpkgs
-      tig
-
-      # Python and Dependencies
-      python312
-      python312Packages.fastapi
-      python312Packages.httpx
-      python312Packages.pip
-      python312Packages.pydantic
-      python312Packages.uv
-      python312Packages.uvicorn
-      poetry
-      # TODO: migrate this to home-manager /home/packages/playwright.nix
-      playwright-driver.browsers
-      playwright-driver
-
-      # System Utilities
-      btop
-      clipse
-      ffmpeg
-      fzf
-      htop
-      p7zip
-      tmux
-      unzip
-      vim
-      wl-clipboard
-      wget
-      xclip
-      xsel # Required for tmux yank
-      yazi
-      zip
-      zoxide
-      zsh
-
-      # Network Tools
-      wgnord # Note: Should install manually from github.com/phirecc/wgnord
-      wireguard-go
-      wireguard-tools
-
-      # javascript
-      nodejs
-      pnpm
-      yarn
+      bash
+      bash-completion
       bun
+      btop
+      cacert
+      clipse
+      curl
+      dbeaver-bin
       deno
-
-      # Applications
-      obs-studio
-      google-chrome
       discord
       (discord.override {
         withOpenASAR = true;
         withVencord = true; # For customization
       })
+      docker
+      ffmpeg
       firefox
-      obsidian
-      postman
-      scrcpy
-      vesktop
-
-      # Terminal Eye Candy
-      pipes
-
-      vim
-      wget
-      curl
+      fzf
+      gh
       git
-      ksnip
-      bash
-      bash-completion
-      sqlite
-      vlc
-      tmux
-      usbutils
+      google-chrome
+      gnumake
       htop
-      wget
-      lshw # see hardware info
       jq
+      just
+      k6
+      ksnip
+      lazydocker
+      lshw
       neofetch
+      nixd
+      nix-prefetch-github
+      nixpkgs-fmt
+      nodejs
+      nodePackages.prettier
+      obs-studio
+      obsidian
+      p7zip
+      pipes
+      pnpm
+      poetry
+      postman
+      python312
+      scrcpy
+      sqlite
+      # terraform # Removed: its broken on nixpkgs
+      tig # git log visualizer
+      tmux
+      unzip
+      usbutils
+      vesktop
+      vim
+      vlc
+      wget
+      wgnord # Note: Should install manually from github.com/phirecc/wgnord
+      wireguard-go
+      wireguard-tools
+      wl-clipboard
+      xclip
+      yazi
+      yarn
+      zip
+      zoxide
+      zsh
     ]
     ++ (with unstable; [
       brave
