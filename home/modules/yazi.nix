792fd8d3813ae6848153c6e14ec3d12eb2ac0f4d
Castrozan <castro.lucas290@gmail.com>
2026-01-23 16:43:52 -0300

    feat: yazi configs

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -16,15 +16,321 @@ _: {
         ];
         show_symlink = true;
       };
+
+      # Configure the which-key popup (shows key sequences)
+      which = {
+        sort_by = "key"; # Sort by key for easier scanning
+        sort_sensitive = false;
+        sort_reverse = false;
+        sort_translit = false;
+      };
     };
 
-    # Keybindings
+    # Keybindings with descriptive labels for the help menu
     keymap = {
+      # Manager layer - main file navigation
       mgr.prepend_keymap = [
+        # === HELP ===
         {
           on = "?";
           run = "help";
-          desc = "Open help";
+          desc = "Show keybindings help (filter by typing)";
+        }
+        {
+          on = "<F1>";
+          run = "help";
+          desc = "Show keybindings help";
+        }
+
+        # === QUICK NAVIGATION ===
+        {
+          on = [
+            "g"
+            "h"
+          ];
+          run = "cd ~";
+          desc = "Go to home directory";
+        }
+        {
+          on = [
+            "g"
+            "c"
+          ];
+          run = "cd ~/.config";
+          desc = "Go to ~/.config";
+        }
+        {
+          on = [
+            "g"
+            "d"
+          ];
+          run = "cd ~/.dotfiles";
+          desc = "Go to dotfiles";
+        }
+        {
+          on = [
+            "g"
+            "D"
+          ];
+          run = "cd ~/Downloads";
+          desc = "Go to Downloads";
+        }
+        {
+          on = [
+            "g"
+            "p"
+          ];
+          run = "cd ~/projects";
+          desc = "Go to projects";
+        }
+        {
+          on = [
+            "g"
+            "t"
+          ];
+          run = "cd /tmp";
+          desc = "Go to /tmp";
+        }
+
+        # === FILE OPERATIONS (with clear descriptions) ===
+        {
+          on = "y";
+          run = "yank";
+          desc = "Yank (copy) selected files";
+        }
+        {
+          on = "x";
+          run = "yank --cut";
+          desc = "Cut selected files";
+        }
+        {
+          on = "p";
+          run = "paste";
+          desc = "Paste yanked files";
+        }
+        {
+          on = "P";
+          run = "paste --force";
+          desc = "Paste (overwrite existing)";
+        }
+        {
+          on = "d";
+          run = "remove";
+          desc = "Move to trash";
+        }
+        {
+          on = "D";
+          run = "remove --permanently";
+          desc = "Delete permanently (careful!)";
+        }
+
+        # === COPY PATH TO CLIPBOARD ===
+        {
+          on = [
+            "c"
+            "c"
+          ];
+          run = "copy path";
+          desc = "Copy file path to clipboard";
+        }
+        {
+          on = [
+            "c"
+            "d"
+          ];
+          run = "copy dirname";
+          desc = "Copy directory path to clipboard";
+        }
+        {
+          on = [
+            "c"
+            "f"
+          ];
+          run = "copy filename";
+          desc = "Copy filename to clipboard";
+        }
+        {
+          on = [
+            "c"
+            "n"
+          ];
+          run = "copy name_without_ext";
+          desc = "Copy filename without extension";
+        }
+
+        # === SELECTION ===
+        {
+          on = "<Space>";
+          run = [
+            "select --state=none"
+            "arrow 1"
+          ];
+          desc = "Toggle selection and move down";
+        }
+        {
+          on = "v";
+          run = "visual_mode";
+          desc = "Enter visual selection mode";
+        }
+        {
+          on = "V";
+          run = "visual_mode --unset";
+          desc = "Exit visual mode";
+        }
+        {
+          on = "<C-a>";
+          run = "select_all --state=true";
+          desc = "Select all files";
+        }
+        {
+          on = "<C-r>";
+          run = "select_all --state=none";
+          desc = "Invert selection";
+        }
+
+        # === VIEW/FILTER ===
+        {
+          on = ".";
+          run = "hidden toggle";
+          desc = "Toggle hidden files";
+        }
+        {
+          on = "/";
+          run = "find --smart";
+          desc = "Find files (fuzzy)";
+        }
+        {
+          on = "f";
+          run = "filter --smart";
+          desc = "Filter files (live)";
+        }
+        {
+          on = "s";
+          run = "search fd";
+          desc = "Search with fd";
+        }
+        {
+          on = "S";
+          run = "search rg";
+          desc = "Search file contents with ripgrep";
+        }
+
+        # === SORTING ===
+        {
+          on = [
+            "o"
+            "m"
+          ];
+          run = "sort modified --reverse";
+          desc = "Sort by modified time (newest first)";
+        }
+        {
+          on = [
+            "o"
+            "M"
+          ];
+          run = "sort modified";
+          desc = "Sort by modified time (oldest first)";
+        }
+        {
+          on = [
+            "o"
+            "n"
+          ];
+          run = "sort natural";
+          desc = "Sort by name (natural)";
+        }
+        {
+          on = [
+            "o"
+            "N"
+          ];
+          run = "sort natural --reverse";
+          desc = "Sort by name (reverse)";
+        }
+        {
+          on = [
+            "o"
+            "s"
+          ];
+          run = "sort size --reverse";
+          desc = "Sort by size (largest first)";
+        }
+        {
+          on = [
+            "o"
+            "e"
+          ];
+          run = "sort extension";
+          desc = "Sort by extension";
+        }
+
+        # === TABS ===
+        {
+          on = "t";
+          run = "tab_create --current";
+          desc = "Create new tab in current dir";
+        }
+        {
+          on = "T";
+          run = "tab_create";
+          desc = "Create new tab in home dir";
+        }
+        {
+          on = "<C-c>";
+          run = "tab_close";
+          desc = "Close current tab";
+        }
+        {
+          on = "[";
+          run = "tab_switch -1 --relative";
+          desc = "Switch to previous tab";
+        }
+        {
+          on = "]";
+          run = "tab_switch 1 --relative";
+          desc = "Switch to next tab";
+        }
+
+        # === SHELL INTEGRATION ===
+        {
+          on = "!";
+          run = "shell --interactive";
+          desc = "Run shell command";
+        }
+        {
+          on = "e";
+          run = "shell --block --confirm '$EDITOR \"$@\"'";
+          desc = "Edit in $EDITOR";
+        }
+        {
+          on = "E";
+          run = "shell --block --confirm 'code \"$@\"'";
+          desc = "Open in VS Code";
+        }
+        {
+          on = "<C-z>";
+          run = "suspend";
+          desc = "Suspend yazi (fg to resume)";
+        }
+      ];
+
+      # Help layer - navigation within help menu
+      help.prepend_keymap = [
+        {
+          on = "/";
+          run = "filter";
+          desc = "Filter keybindings";
+        }
+        {
+          on = "<Esc>";
+          run = "escape";
+          desc = "Clear filter / close help";
+        }
+        {
+          on = "q";
+          run = "close";
+          desc = "Close help";
         }
       ];
     };
