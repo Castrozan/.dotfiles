76b3834d54207cb01161f1d10ca20f305a739581
Castrozan <castro.lucas290@gmail.com>
2026-01-25 20:13:50 -0300

    test: add hyprland to home.packages to replicate issue

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ pkgs, inputs, ... }:
+let
+  hyprlandFlake = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+in
 {
   home.file.".config/hypr".source = ../../../.config/hypr;
+
+  home.packages = [
+    hyprlandFlake
+  ];
 }
