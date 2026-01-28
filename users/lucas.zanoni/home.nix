06d7beb3a662c586eed2457ee3350e67d1d630b6
Castrozan <castro.lucas290@gmail.com>
2026-01-27 22:34:05 -0300

    feat(lucas.zanoni): vesktop

--- users/lucas.zanoni/home.nix
+++ users/lucas.zanoni/home.nix
@@ -49,6 +49,7 @@
     ../../home/modules/tui-notifier.nix
     ../../home/modules/vial.nix
     ../../home/modules/vscode
+    ../../home/modules/vesktop.nix
     ../../home/modules/yazi.nix
     ../../home/modules/wezterm.nix # https://tmuxai.dev/terminal-compatibility/
     ../../home/modules/whisper-input
