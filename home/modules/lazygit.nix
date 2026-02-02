f4825191b8eab8a7b83146b442ab90779b6c2179
Castrozan <castro.lucas290@gmail.com>
2026-02-02 20:46:34 -0300

    feat(lazygit): add shift+I quick-commit custom command

--- home/modules/lazygit.nix
+++ home/modules/lazygit.nix
@@ -5,23 +5,18 @@
 
     settings = {
       os = {
-        # Run commands in an interactive shell to load aliases
         shell = "${pkgs.fish}/bin/fish -i -c";
       };
-    };
 
-    # TODO: this is not working, so we're disabling it for now
-    # The user config file /home/zanoni/.config/lazygit/config.yml must be migrated. Attempting to do this automatically.
-    # The following changes were made:
-    # - Moved git.paging object to git.pagers array
-    # 2026/01/06 02:21:17 While attempting to write back migrated user config to /home/zanoni/.config/lazygit/config.yml, an error occurred: open /home/zanoni/.config/lazygit/config.yml: read-only file system
-    # settings = {
-    #   git = {
-    #     paging = {
-    #       colorArg = "always";
-    #       pager = "delta --paging=never --detect-dark-light always";
-    #     };
-    #   };
-    # };
+      customCommands = [
+        {
+          key = "I";
+          context = "global";
+          description = "Quick-commit dotfiles + private-config submodule";
+          command = ''dotfiles-quick-commit'';
+          showOutput = true;
+        }
+      ];
+    };
   };
 }
