f0eb3613ce39c91735f555239496a956d22b178b
Castrozan <castro.lucas290@gmail.com>
2026-02-09 14:13:26 -0300

    fix(zed): use nixpkgs-unstable instead of flake input
    The zed flake uses builtins.fetchGit for cargo git deps at evaluation
    time, bypassing flake.lock. This causes rebuilds to fail when GitHub
    is down. Switch to nixpkgs-unstable which uses fixed-output derivations.

--- shell/screensaver.sh
+++ shell/screensaver.sh
@@ -2,6 +2,8 @@
 
 # Source screensaver configuration and core functions
 # shellcheck source=shell/screensaver/config.sh
+# shellcheck disable=SC1091
 source ~/.dotfiles/shell/screensaver/config.sh
 # shellcheck source=shell/screensaver/core.sh
+# shellcheck disable=SC1091
 source ~/.dotfiles/shell/screensaver/core.sh
