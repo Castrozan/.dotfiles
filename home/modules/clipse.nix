374787b2f0c8270ce4b44257a2bf2348ade72531
Castrozan <castro.lucas290@gmail.com>
2026-02-10 11:09:31 -0300

    feat(clipse): manage config via home-manager with kitty image protocol
    - Move config.json to xdg.configFile in clipse.nix module
    - Use kitty graphics protocol instead of basic unicode blocks
    - Point themeFile directly to hypr-theme/current instead of copy
    - Remove redundant clipse theme copy step from theme-set

--- home/modules/clipse.nix
+++ home/modules/clipse.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   # Custom fork of clipse
   clipse-zanoni = pkgs.buildGoModule {
@@ -31,15 +31,47 @@ let
   };
 in
 {
-  # NOTE: On GNOME Wayland, wl-paste --watch doesn't work (requires wlroots protocol)
-  # See docs/clipse-gnome-issues.md for details and alternatives
   home.packages = [
     pkgs.wl-clipboard
     clipse-zanoni
   ];
 
-  # Background clipboard listener service
-  # Works on wlroots compositors (Hyprland, Sway), limited on GNOME
+  xdg.configFile."clipse/config.json".text = builtins.toJSON {
+    historyFile = "clipboard_history.json";
+    maxHistory = 100;
+    allowDuplicates = false;
+    themeFile = "${config.home.homeDirectory}/.config/hypr-theme/current/theme/clipse.json";
+    tempDir = "tmp_files";
+    logFile = "clipse.log";
+    keyBindings = {
+      choose = "enter";
+      clearSelected = "S";
+      down = "down";
+      end = "end";
+      filter = "/";
+      home = "home";
+      more = "?";
+      nextPage = "right";
+      prevPage = "left";
+      preview = "t";
+      quit = "q";
+      remove = "x";
+      selectDown = "ctrl+down";
+      selectSingle = "s";
+      selectUp = "ctrl+up";
+      togglePin = "p";
+      togglePinned = "tab";
+      up = "up";
+      yankFilter = "ctrl+s";
+    };
+    imageDisplay = {
+      type = "kitty";
+      scaleX = 9;
+      scaleY = 9;
+      heightCut = 2;
+    };
+  };
+
   systemd.user.services.clipse = {
     Unit = {
       Description = "Clipse clipboard manager listener";
