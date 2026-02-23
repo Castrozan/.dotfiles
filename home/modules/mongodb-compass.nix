a10be9e7f4c9e110ed01f6932e8294e97b31aa4c
Castrozan <castro.lucas290@gmail.com>
2026-02-23 16:38:14 -0300

    fix(mongodb-compass): wrap with nixGLIntel for non-NixOS GPU access
    MongoDB Compass failed to launch from Fuzzel because it lacked Mesa/GPU
    library paths that terminal sessions inherit from nixGL-wrapped wezterm.

--- home/modules/mongodb-compass.nix
+++ home/modules/mongodb-compass.nix
@@ -1,11 +1,34 @@
-{ pkgs, ... }:
 {
-  home.packages = [ pkgs.mongodb-compass ];
+  pkgs,
+  inputs,
+  isNixOS,
+  ...
+}:
+let
+  mongodbCompassPackage =
+    if isNixOS then
+      pkgs.mongodb-compass
+    else
+      let
+        nixGLWrapper = inputs.nixgl.packages.${pkgs.stdenv.hostPlatform.system}.nixGLIntel;
+        mongodbCompassWithNixGL = pkgs.writeShellScriptBin "mongodb-compass" ''
+          exec ${nixGLWrapper}/bin/nixGLIntel ${pkgs.mongodb-compass}/bin/mongodb-compass "$@"
+        '';
+      in
+      pkgs.symlinkJoin {
+        name = "mongodb-compass-wrapped";
+        paths = [
+          mongodbCompassWithNixGL
+          pkgs.mongodb-compass
+        ];
+      };
+in
+{
+  home.packages = [ mongodbCompassPackage ];
 
   xdg.desktopEntries.mongodb-compass = {
     name = "MongoDB Compass";
     genericName = "MongoDB GUI";
-    comment = "The official GUI for MongoDB";
     exec = "mongodb-compass %U";
     icon = "mongodb-compass";
     terminal = false;
