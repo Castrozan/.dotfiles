6219479df2f625ef4181159f405a6884f328d598
Castrozan <castro.lucas290@gmail.com>
2026-02-23 21:19:28 -0300

    fix(jetbrains): force Wayland toolkit and disable splash screen

--- home/modules/jetbrains-idea.nix
+++ home/modules/jetbrains-idea.nix
@@ -1,30 +1,19 @@
-{
-  pkgs,
-  isNixOS,
-  ...
-}:
+{ pkgs, ... }:
 let
-  ideaPackage = pkgs.jetbrains.idea;
-
-  ideaWaylandVmOptions = pkgs.writeText "idea-wayland.vmoptions" ''
-    -Dawt.toolkit.name=WLToolkit
-  '';
-
-  ideaWithWayland = pkgs.writeShellScriptBin "idea" ''
-    export IDEA_VM_OPTIONS="${ideaWaylandVmOptions}"
-    exec ${ideaPackage}/bin/idea "$@"
-  '';
-
-  ideaWrapped = pkgs.symlinkJoin {
-    name = "jetbrains-idea-wrapped";
-    paths = [
-      ideaWithWayland
-      ideaPackage
-    ];
-  };
-
-  finalPackage = if isNixOS then ideaPackage else ideaWrapped;
+  intellijWithSplashDisabledAndWayland = pkgs.jetbrains.idea.overrideAttrs (previousAttrs: {
+    postPatch = (previousAttrs.postPatch or "") + ''
+      substituteInPlace bin/idea.sh \
+        --replace-fail '-Dsplash=true' '-Dsplash=false'
+    '';
+    postInstall = (previousAttrs.postInstall or "") + ''
+      rm "$out/bin/idea"
+      ln -s "$out/idea/bin/idea.sh" "$out/bin/idea"
+    '';
+  });
 in
 {
-  home.packages = [ finalPackage ];
+  home.packages = [ intellijWithSplashDisabledAndWayland ];
+
+  xdg.configFile."JetBrains/IntelliJIdea2025.2/idea64.vmoptions".text =
+    "-Dawt.toolkit.name=WLToolkit\n";
 }
