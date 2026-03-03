4c949f9609d42a3c1b787ae1f9dd0aafa1c18900
Castrozan <castro.lucas290@gmail.com>
2026-03-03 00:52:27 -0300

    chore(jetbrains): simplify IntelliJ IDEA configuration

--- home/modules/jetbrains-idea.nix
+++ home/modules/jetbrains-idea.nix
@@ -1,19 +1,10 @@
 { pkgs, ... }:
-let
-  intellijWithSplashDisabledAndWayland = pkgs.jetbrains.idea.overrideAttrs (previousAttrs: {
-    postPatch = (previousAttrs.postPatch or "") + ''
-      substituteInPlace bin/idea.sh \
-        --replace-fail '-Dsplash=true' '-Dsplash=false'
-    '';
-    postInstall = (previousAttrs.postInstall or "") + ''
-      rm "$out/bin/idea"
-      ln -s "$out/idea/bin/idea.sh" "$out/bin/idea"
-    '';
-  });
-in
 {
-  home.packages = [ intellijWithSplashDisabledAndWayland ];
+  home.packages = [ pkgs.jetbrains.idea ];
 
-  xdg.configFile."JetBrains/IntelliJIdea2025.2/idea64.vmoptions".text =
-    "-Dawt.toolkit.name=WLToolkit\n";
+  xdg.configFile."JetBrains/IntelliJIdea2025.2/idea64.vmoptions".text = ''
+    -Dawt.toolkit.name=WLToolkit
+    -Dsplash=false
+  '';
+
 }
