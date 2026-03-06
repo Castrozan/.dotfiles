b02d7361a6fcb7e0b86739450d6587278a35aa5f
Castrozan <castro.lucas290@gmail.com>
2026-03-06 17:41:59 -0300

    fix(firefox): use normal_installed for Bitwarden to avoid slowdown warning
    fix(firefox): remove Bitwarden extension
    
    fix(firefox): remove SponsorBlock, Facebook Container, Multi-Account Containers

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -53,28 +53,13 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
         };
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
+
         "jid1-MnnxcxisBPnSXQ@jetpack" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
           installation_mode = "force_installed";
         };
         "addon@darkreader.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "sponsorBlocker@ajay.app" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "@contain-facebook" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/facebook-container/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "@testpilot-containers" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
           installation_mode = "force_installed";
         };
       };
