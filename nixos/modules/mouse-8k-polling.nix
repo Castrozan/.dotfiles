83063b4a7cc8982c941e513f53160c02a976199f
Castrozan <castro.lucas290@gmail.com>
2026-03-07 13:23:38 -0300

    fix(mouse): use GROUP=input instead of TAG=uaccess for hidraw access
    TAG+="uaccess" relies on logind ACLs which don't propagate on udevadm
    trigger. GROUP="input" is reliable since user is already in the group.

--- nixos/modules/mouse-8k-polling.nix
+++ nixos/modules/mouse-8k-polling.nix
@@ -1,7 +1,7 @@
 { username, pkgs, ... }:
 {
   services.udev.extraRules = ''
-    KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="1040", MODE="0660", TAG+="uaccess"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="1040", MODE="0660", GROUP="input"
   '';
 
   users.users.${username}.extraGroups = [ "input" ];
