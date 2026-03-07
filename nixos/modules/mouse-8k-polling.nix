2008448e5fc4a1d6ff2ae2b5075d5bd5a3f455ef
Castrozan <castro.lucas290@gmail.com>
2026-03-07 13:38:10 -0300

    fix(mouse): correct product ID from 1040 to 103f for Compx MAD R dongle

--- nixos/modules/mouse-8k-polling.nix
+++ nixos/modules/mouse-8k-polling.nix
@@ -1,7 +1,7 @@
 { username, pkgs, ... }:
 {
   services.udev.extraRules = ''
-    KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="1040", MODE="0660", GROUP="input"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="103f", MODE="0660", GROUP="input"
   '';
 
   users.users.${username}.extraGroups = [ "input" ];
