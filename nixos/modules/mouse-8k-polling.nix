10ccd4e12b4185957140f18ab7de074319cde3ae
Castrozan <castro.lucas290@gmail.com>
2026-03-07 14:02:50 -0300

    fix(mouse): add udev rule for 8K dongle mode (PID 1040)
    Dongle re-enumerates as MAD 8K DONGLE with different PID
    when EEPROM is set to 8000Hz polling rate.

--- nixos/modules/mouse-8k-polling.nix
+++ nixos/modules/mouse-8k-polling.nix
@@ -2,6 +2,7 @@
 {
   services.udev.extraRules = ''
     KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="103f", MODE="0660", GROUP="input"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="1040", MODE="0660", GROUP="input"
   '';
 
   users.users.${username}.extraGroups = [ "input" ];
