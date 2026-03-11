a0b7e9097beb77d7966672e6c808bec36a8efa5a
Castrozan <castro.lucas290@gmail.com>
2026-03-10 21:16:52 -0300

    feat(dev): migrate git-toggle-user from bash to python 3.12

--- users/lucas.zanoni/scripts/git-toggle-user.nix
+++ users/lucas.zanoni/scripts/git-toggle-user.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 let
-  git-toggle-user = pkgs.writeShellScriptBin "git-toggle-user" (
-    builtins.readFile ../../../home/modules/dev/scripts/git-toggle-user
+  pythonSource = pkgs.writeText "git-toggle-user-source.py" (
+    builtins.readFile ../../../home/modules/dev/scripts/git_toggle_user.py
   );
+  git-toggle-user = pkgs.writeShellScriptBin "git-toggle-user" ''
+    exec ${pkgs.python312}/bin/python3 ${pythonSource} "$@"
+  '';
 in
 {
   home.packages = [ git-toggle-user ];
