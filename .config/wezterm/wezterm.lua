6895f1e6e2a1f751b0181230de365efc8619fcb5
Castrozan <castro.lucas290@gmail.com>
2026-03-16 22:54:08 -0300

    fix(wezterm): raise background opacity to 0.85 to prevent invisible window glitch

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -65,7 +65,7 @@ return {
   max_fps = 120,
   window_decorations = 'RESIZE',
   use_resize_increments = false,
-  window_background_opacity = 0.6,
+  window_background_opacity = 0.85,
   macos_window_background_blur = 20,
   enable_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
