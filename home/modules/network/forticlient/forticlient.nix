d1c3618c9909e160edea9d3be1f4b6ab85ab7baf
Castrozan <castro.lucas290@gmail.com>
2026-03-26 18:38:08 -0300

    style(network): merge repeated xdg keys in forticlient module

--- home/modules/network/forticlient/forticlient.nix
+++ home/modules/network/forticlient/forticlient.nix
@@ -14,42 +14,46 @@ in
     forticlientAuthCallbackScript
   ];
 
-  xdg.desktopEntries.forticlient-launch = {
-    name = "FortiClient";
-    genericName = "VPN Client";
-    exec = "forticlient-launch";
-    icon = "forticlient";
-    terminal = true;
-    type = "Application";
-    categories = [
-      "Network"
-      "Security"
-    ];
-  };
+  xdg = {
+    desktopEntries = {
+      forticlient-launch = {
+        name = "FortiClient";
+        genericName = "VPN Client";
+        exec = "forticlient-launch";
+        icon = "forticlient";
+        terminal = true;
+        type = "Application";
+        categories = [
+          "Network"
+          "Security"
+        ];
+      };
 
-  xdg.desktopEntries.forticlient-auth-handler = {
-    name = "FortiClient Auth Handler";
-    exec = "forticlient-auth-callback %u";
-    noDisplay = true;
-    type = "Application";
-    mimeType = [ "x-scheme-handler/fabricagent" ];
-  };
+      forticlient-auth-handler = {
+        name = "FortiClient Auth Handler";
+        exec = "forticlient-auth-callback %u";
+        noDisplay = true;
+        type = "Application";
+        mimeType = [ "x-scheme-handler/fabricagent" ];
+      };
 
-  xdg.desktopEntries.forticlient = {
-    name = "FortiClient (dpkg)";
-    exec = "forticlient-launch";
-    noDisplay = true;
-    type = "Application";
-  };
+      forticlient = {
+        name = "FortiClient (dpkg)";
+        exec = "forticlient-launch";
+        noDisplay = true;
+        type = "Application";
+      };
 
-  xdg.desktopEntries.forticlient-register = {
-    name = "FortiClient Register (dpkg)";
-    exec = "forticlient-launch";
-    noDisplay = true;
-    type = "Application";
-  };
+      forticlient-register = {
+        name = "FortiClient Register (dpkg)";
+        exec = "forticlient-launch";
+        noDisplay = true;
+        type = "Application";
+      };
+    };
 
-  xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/fabricagent" = "forticlient-auth-handler.desktop";
+    mimeApps.defaultApplications = {
+      "x-scheme-handler/fabricagent" = "forticlient-auth-handler.desktop";
+    };
   };
 }
