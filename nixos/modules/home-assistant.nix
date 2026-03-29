d0a31b53f718b4710641de99732ebc2eef46cf66
Castrozan <castro.lucas290@gmail.com>
2026-03-29 18:20:17 -0300

    fix(home-assistant): install midea-local pip package on container start

--- nixos/modules/home-assistant.nix
+++ nixos/modules/home-assistant.nix
@@ -1,7 +1,15 @@
 {
   username,
+  pkgs,
   ...
 }:
+let
+  installMideaLocalInContainer = pkgs.writeShellScript "install-midea-local-in-homeassistant" ''
+    sleep 10
+    ${pkgs.podman}/bin/podman exec homeassistant pip install --quiet midea-local==6.5.0
+    ${pkgs.podman}/bin/podman exec homeassistant python -c "import midealocal" || exit 1
+  '';
+in
 {
   virtualisation.oci-containers.containers.homeassistant = {
     image = "ghcr.io/home-assistant/home-assistant:stable";
@@ -10,4 +18,6 @@
     ];
     extraOptions = [ "--network=host" ];
   };
+
+  systemd.services.podman-homeassistant.serviceConfig.ExecStartPost = installMideaLocalInContainer;
 }
