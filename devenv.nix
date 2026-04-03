dc8e956725be13860e76d96a7dae9acaea626806
Castrozan <castro.lucas290@gmail.com>
2026-04-03 01:24:39 -0300

    feat(devenv): configure PostgreSQL service with dev database and Python 3.12

--- devenv.nix
+++ devenv.nix
@@ -5,4 +5,13 @@
     pkgs.python312
     pkgs.postgresql_16
   ];
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    initialDatabases = [ { name = "dev"; } ];
+  };
+
+  env.PGHOST = "localhost";
+  env.PGUSER = "postgres";
 }
