09d5e915f453713ca5e4abb4759e28b378e74480
Castrozan <castro.lucas290@gmail.com>
2026-05-06 15:10:25 -0300

    feat(network): neutralize forticlient periodic update binary
    fctsched periodically spawns /opt/forticlient/update which spikes CPU to ~85%; replace it with a no-op shim, keeping the original at update.original. Mirrors sophos-disable-plugins pattern: oneshot service runs after forticlient.service.

---
+++ users/lucas.zanoni/home/forticlient-disable-update/default.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+let
+  dotfilesDir = "${config.home.homeDirectory}/.dotfiles";
+  installerScriptPath = "${dotfilesDir}/users/lucas.zanoni/home/forticlient-disable-update/install-systemd-service";
+  disableScriptSourcePath = "${dotfilesDir}/users/lucas.zanoni/home/forticlient-disable-update/disable-forticlient-update";
+  installedSystemdUnitPath = "/etc/systemd/system/forticlient-disable-update.service";
+  installedDisableScriptPath = "/usr/local/sbin/forticlient-disable-update";
+in
+{
+  home.activation.setupForticlientDisableUpdate = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    PATH="/usr/bin:/usr/sbin:/sbin:$PATH"
+
+    if ! systemctl list-unit-files forticlient.service --no-legend 2>/dev/null | grep -q forticlient; then
+      $VERBOSE_ECHO "forticlient.service not present, skipping update neutralizer"
+    elif [ -f "${installedSystemdUnitPath}" ] \
+      && [ -x "${installedDisableScriptPath}" ] \
+      && cmp -s "${disableScriptSourcePath}" "${installedDisableScriptPath}" \
+      && systemctl is-enabled --quiet forticlient-disable-update.service 2>/dev/null; then
+      $VERBOSE_ECHO "forticlient-disable-update.service already installed and matches source"
+    else
+      echo "Installing forticlient-disable-update.service (requires sudo)..."
+      sudo "${installerScriptPath}"
+    fi
+  '';
+}
