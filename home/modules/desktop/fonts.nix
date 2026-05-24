1a1fd9f02fd81b364c34a35cfe515104be1edc63
Castrozan <castro.lucas290@gmail.com>
2026-05-24 01:09:13 -0300

    fonts: keep all font packages in one shared list
    Reverts the platform-conditional split of home.packages from the
    previous commit - the font packages themselves build fine on darwin.
    Only fonts.fontconfig.enable needs the isLinux guard (fontconfig is a
    Linux-only service; darwin uses CoreText).

--- home/modules/desktop/fonts.nix
+++ home/modules/desktop/fonts.nix
@@ -1,21 +1,15 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   fonts.fontconfig.enable = pkgs.stdenv.hostPlatform.isLinux;
 
-  home.packages =
-    (with pkgs; [
-      nerd-fonts.monaspace
-      nerd-fonts.fira-code
-      nerd-fonts.jetbrains-mono
-      fira-code
-      fira-code-symbols
-    ])
-    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux (
-      with pkgs;
-      [
-        noto-fonts-color-emoji
-        symbola
-        material-symbols
-      ]
-    );
+  home.packages = with pkgs; [
+    nerd-fonts.monaspace
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    fira-code
+    fira-code-symbols
+    noto-fonts-color-emoji
+    symbola
+    material-symbols
+  ];
 }
