0d83e7e50b0ac881bab68cdaa2ff713bd7f1b5b0
Castrozan <castro.lucas290@gmail.com>
2026-05-29 22:16:49 -0300

    feat(nvim): restore lost keybinds from old dotfiles-macbook
    Brings back C-Up/Down viewport scroll, C-PageUp/Down bufferline cycling,
    the smart close-buffer (focus next then prev) on <leader>c and C-w, the
    <leader>r config reload, and require('config.file_explorer') for the
    persisted-width Snacks explorer on C-S-b. Keeps C-b -> Snacks.explorer.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,9 +22,22 @@ end, { desc = "Workspace symbols" })
 map("n", "<C-/>", "gcc", { remap = true, desc = "Toggle comment" })
 map("v", "<C-/>", "gc", { remap = true, desc = "Toggle comment (visual)" })
 
+map("n", "<leader>r", function()
+  for module_name, _ in pairs(package.loaded) do
+    if module_name:match("^config%.") then
+      package.loaded[module_name] = nil
+    end
+  end
+  vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/config/keymaps.lua")
+  vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/config/options.lua")
+  vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/config/autocmds.lua")
+  vim.notify("nvim config reloaded", vim.log.levels.INFO)
+end, { desc = "Reload nvim config (keymaps/options/autocmds)" })
+
 map("n", "<C-b>", function()
   Snacks.explorer()
 end, { desc = "Toggle file explorer" })
+require("config.file_explorer")
 map("n", "<C-`>", function()
   Snacks.terminal()
 end, { desc = "Toggle terminal" })
@@ -36,6 +49,13 @@ map("n", "<S-F12>", function()
 end, { desc = "Find references" })
 map("n", "<C-.>", vim.lsp.buf.code_action, { desc = "Code action" })
 map({ "n", "i" }, "<C-S-Space>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+
+map("n", "<C-Up>", "<C-y>", { desc = "Scroll view up one line" })
+map("n", "<C-Down>", "<C-e>", { desc = "Scroll view down one line" })
+map("i", "<C-Up>", "<C-o><C-y>", { desc = "Scroll view up one line" })
+map("i", "<C-Down>", "<C-o><C-e>", { desc = "Scroll view down one line" })
+map("v", "<C-Up>", "<C-y>", { desc = "Scroll view up one line" })
+map("v", "<C-Down>", "<C-e>", { desc = "Scroll view down one line" })
 
 map("n", "<C-Right>", "w", { desc = "Jump to next word" })
 map("n", "<C-Left>", "b", { desc = "Jump to previous word" })
@@ -51,13 +71,56 @@ map("i", "<C-S-Up>", "<esc>10kzzi", { desc = "Jump 10 lines up" })
 map("v", "<C-S-Down>", "10jzz", { desc = "Jump 10 lines down" })
 map("v", "<C-S-Up>", "10kzz", { desc = "Jump 10 lines up" })
 
+map("n", "<C-PageUp>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous open file" })
+map("n", "<C-PageDown>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next open file" })
+map("i", "<C-PageUp>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous open file" })
+map("i", "<C-PageDown>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next open file" })
+
 map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
 map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
 map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
 map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
 
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
-map("n", "<leader>c", "<cmd>bd<cr>", { desc = "Close buffer" })
+
+local function find_other_listed_file_buffer_than(excluded_buffer_id)
+  for _, buffer_id in ipairs(vim.api.nvim_list_bufs()) do
+    if
+      buffer_id ~= excluded_buffer_id
+      and vim.api.nvim_buf_is_valid(buffer_id)
+      and vim.bo[buffer_id].buflisted
+      and vim.api.nvim_buf_get_name(buffer_id) ~= ""
+    then
+      return buffer_id
+    end
+  end
+  return nil
+end
+
+local function current_buffer_represents_a_file()
+  return vim.bo.buflisted and vim.api.nvim_buf_get_name(0) ~= ""
+end
+
+local function close_current_buffer_focusing_right_then_left()
+  local buffer_to_close = vim.api.nvim_get_current_buf()
+  if current_buffer_represents_a_file() and not find_other_listed_file_buffer_than(buffer_to_close) then
+    Snacks.dashboard({ win = vim.api.nvim_get_current_win() })
+    vim.cmd("bdelete " .. buffer_to_close)
+    return
+  end
+  vim.cmd("BufferLineCycleNext")
+  if vim.api.nvim_get_current_buf() == buffer_to_close then
+    vim.cmd("BufferLineCyclePrev")
+  end
+  vim.cmd("bdelete " .. buffer_to_close)
+end
+
+map("n", "<leader>c", close_current_buffer_focusing_right_then_left, { desc = "Close buffer" })
+map("n", "<C-w>", close_current_buffer_focusing_right_then_left, { desc = "Close buffer (focus next or prev)" })
+map("i", "<C-w>", function()
+  vim.cmd("stopinsert")
+  close_current_buffer_focusing_right_then_left()
+end, { desc = "Close buffer (focus next or prev)" })
 
 map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 map("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
