a2b90d5b499972847e57c3fd0a9ef2f4be623ea7
Castrozan <castro.lucas290@gmail.com>
2026-08-05 13:36:22 -0300

    fix(chise): merge the man-cache systemd keys statix flags as repeated

--- machine-configuration/operating-system/manual-pages/man-cache-nixos.nix
+++ machine-configuration/operating-system/manual-pages/man-cache-nixos.nix
@@ -2,22 +2,24 @@
 {
   documentation.man.generateCaches = false;
 
-  systemd.tmpfiles.rules = [ "d /var/cache/man/nixos 0755 root root -" ];
+  systemd = {
+    tmpfiles.rules = [ "d /var/cache/man/nixos 0755 root root -" ];
 
-  systemd.services.update-man-db = {
-    description = "Update man-db cache";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.man-db}/bin/mandb";
+    services.update-man-db = {
+      description = "Update man-db cache";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.man-db}/bin/mandb";
+      };
     };
-  };
 
-  systemd.timers.update-man-db = {
-    description = "Timer for updating man-db cache";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
+    timers.update-man-db = {
+      description = "Timer for updating man-db cache";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
     };
   };
 }
